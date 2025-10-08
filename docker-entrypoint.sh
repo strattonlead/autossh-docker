30f2f722dd50a94e68d02e391485c935c17d8a53
Arndt Bieberstein <arndt.bieberstein@gmail.com>
2025-10-08 12:02:32 +0200

    logs

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -6,6 +6,7 @@ mkdir -p /root/.ssh
 chmod 700 /root/.ssh
 
 if [ -n "$SSH_KEY" ]; then
+  echo "[custom] saving key to /root/.ssh/id_rsa"
   echo "$SSH_KEY" > /root/.ssh/id_rsa
   chmod 600 /root/.ssh/id_rsa
 fi
