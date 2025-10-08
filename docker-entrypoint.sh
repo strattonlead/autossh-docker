4b176f907122a96648a0d1308dbdf68ec76b1526
Arndt Bieberstein <arndt.bieberstein@gmail.com>
2025-10-08 11:49:42 +0200

    custom entrypoint

---
+++ docker-entrypoint.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+set -e
+
+echo "[custom] preparing key…"
+mkdir -p /root/.ssh
+chmod 700 /root/.ssh
+
+if [ -n "$SSH_KEY" ]; then
+  echo "$SSH_KEY" > /root/.ssh/id_rsa
+  chmod 600 /root/.ssh/id_rsa
+fi
+
+echo "[custom] launching original autossh entrypoint..."
+exec /entrypoint "$@"
